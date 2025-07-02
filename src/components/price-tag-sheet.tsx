c502f2122329df1796c0937f02893dce4d979175
Subhrajit Sen Choudhury <Subhrajit096@gmail.com>
2025-07-02 19:52:26 +0000

    reduce the black space bwtween QR code and logo

--- src/components/price-tag-sheet.tsx
+++ src/components/price-tag-sheet.tsx
@@ -26,7 +26,7 @@ const PriceTag = ({ product }: { product: Product }) => (
       <RoopkothaLogo showTagline={true} width={150} height={36} />
     </div>
 
-    <div style={{ margin: '1px 0' }}>
+    <div style={{ margin: '0' }}>
       <QRCodeCanvas
         value={product.barcode}
         size={35}
