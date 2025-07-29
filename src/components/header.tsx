a30ad13d263ebe285c685937da153353676352b7
Subhrajit Sen Choudhury <Subhrajit096@gmail.com>
2025-07-29 09:13:48 +0000

    make the whole content fit in the full mobile screen, there are a lot of

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -65,7 +65,7 @@ export default function Header({ addProduct, onImportInventory, userRole, events
 
   return (
     <header className="flex-shrink-0 bg-card border-b shadow-sm sticky top-0 z-10">
-      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+      <div className="px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center">
              <EventSwitcher 
